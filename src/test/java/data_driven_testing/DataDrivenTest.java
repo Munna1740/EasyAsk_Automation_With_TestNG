b3a9f3e7c4a557c4dcb66fd093e2eb7f71602201
MD Mostafa Munna <49121930+Munna1740@users.noreply.github.com>
2021-09-14 15:18:44 +0600

    Data Driven & web table to excel sheet

--- src/test/java/data_driven_testing/DataDrivenTest.java
+++ src/test/java/data_driven_testing/DataDrivenTest.java
@@ -1,5 +1,7 @@
 package data_driven_testing;
 
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
@@ -29,6 +31,8 @@ public class DataDrivenTest extends DriverSetup {
 		WebElement login = driver.findElement(By.className("btn-primary"));
 		login.click();
 		
+	    System.out.println(login);
+		
 		if(exp.equals("valid")) {
 			
 			WebElement account = driver.findElement(By.xpath("//header/ul[2]/li[1]/a[1]"));
@@ -51,14 +55,35 @@ public class DataDrivenTest extends DriverSetup {
 	
 	
 	@DataProvider(name = "LoginData")
-	public String [][] getData() {
+	public String [][] getData() throws IOException {
 		
-		String loginData[][] = {
+		
+		/*String loginData[][] = {
 				{"superadmin@gmail.com", "superadmin", "valid"},
 				{"admin@gmail.com", "admin", "invalid"},
 				{"kamal@gmail.com", "kamal", "invalid"},
 				{"jafar@gmail.com", "jafar", "invalid"}
-		};
+		};*/
+		
+		//get data from excel file
+		String path = ".\\datafiles\\loginData.xlsx";
+		XLUtility xlutil = new XLUtility(path);
+		
+		int totalrows = xlutil.getRowCount("Sheet1");
+		int totalcols = xlutil.getCellCount("Sheet1", 1);
+		
+		String loginData[][] = new String[totalrows][totalcols];
+		
+		for(int i=1;i<=totalrows;i++) //1
+		{
+			for(int j=0;j<totalcols;j++) //0
+			{
+				loginData[i-1][j]=xlutil.getCellData("Sheet1", i, j);
+			}
+				
+		}
+
+		
 		
 		return loginData;
 		
